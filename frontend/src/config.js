8ff92bccceb482bc842d17ce2a39cacb0bbafe5c
Shivam Patro <shivampatro60@gmail.com>
2026-07-21 11:16:23 +0530

    changed config file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || (process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000');
+export const API_BASE_URL = process.env.REACT_APP_API_URL || (process.env.NODE_ENV === 'production' ? 'https://pawtopia-dunz.onrender.com' : 'http://localhost:5000');
+
